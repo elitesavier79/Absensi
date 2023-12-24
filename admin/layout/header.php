55edd14b17376fc718e30b47241eaebfb92b1335
Dicky Swardi <windseven79@gmail.com>
2023-12-24 20:41:58 +0700

    konfigurasi Template Tabler io

--- admin/layout/header.php
+++ admin/layout/header.php
@@ -148,19 +148,16 @@ require_once '../../config.php'
             </div>
             <div class="nav-item dropdown">
               <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown" aria-label="Open user menu">
-                <span class="avatar avatar-sm" style="background-image: url(./static/avatars/000m.jpg)"></span>
+                <span class="avatar avatar-sm" style="background-image: url(../../assets/img/008m.jpg)"></span>
                 <div class="d-none d-xl-block ps-2">
                   <div>Paweł Kuna</div>
                   <div class="mt-1 small text-muted">UI Designer</div>
                 </div>
               </a>
               <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                <a href="#" class="dropdown-item">Status</a>
-                <a href="./profile.html" class="dropdown-item">Profile</a>
-                <a href="#" class="dropdown-item">Feedback</a>
-                <div class="dropdown-divider"></div>
-                <a href="./settings.html" class="dropdown-item">Settings</a>
-                <a href="./sign-in.html" class="dropdown-item">Logout</a>
+                <a href="#" class="dropdown-item">Profile</a>
+                <a href="./profile.html" class="dropdown-item">Ubah Password</a>
+                <a href="#" class="dropdown-item">Logout</a>
               </div>
             </div>
           </div>
